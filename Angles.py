f78d896246370956fd9ea308ce258644d56d66e5
ThanderB <136999608+ThanderB@users.noreply.github.com>
2023-06-20 13:47:33 +0530

    Update Angles.py

--- Angles.py
+++ Angles.py
@@ -1,7 +1,7 @@
 Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
-= RESTART: C:/Users/HP/Desktop/AVO/GitHub/Angle_from_Offset - Angle_Vs_Sine_Sqare_Theta.py
+= RESTART: D:/Angle_from_Offset - Angle_Vs_Sine_Sqare_Theta.py
 Offsets in meter: [ 40 120 200 280 360 440 520 600 680]
 Angle in Degrees: 1.1811645538972224
 Angle in Degrees: 3.5394863378484898
